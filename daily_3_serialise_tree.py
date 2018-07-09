c126680adbe60d8053c9983ba03ab15808f04c2c
Jolan Cornevin <jcornevin@work4labs.com>
2018-07-09 14:14:27 +0200

    first implentation of the daily 3

--- daily_3_serialise_tree.py
+++ daily_3_serialise_tree.py
@@ -17,8 +17,50 @@ assert deserialize(serialize(node)).left.left.val == 'left.left'
 /!\ ASSERTIONS: 
   - I can use regex.
 """
+import re
+
+RE_NODE_PATTERN = re.compile('\[(.*?), (\[.*\]|\(None\)), (\[.*\]|\(None\))\]')
+NO_NODE = '(None)'
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def serialize(node):
+    """
+    Serializes the tree into a string. The format is (val, (right_node), (left_node)) recursivly.
+    """
+    return '[{}, {}, {}]'.format(node.val, serialize(node.right) if node.right else NO_NODE, serialize(node.left) if node.left else NO_NODE)
+
+def extract_val_from_str(str):
+    if not  RE_NODE_PATTERN.match(str):
+        print str
+
+    return RE_NODE_PATTERN.match(str).groups()
+
+def recursive_deserialize(val, right, left):
+    right = recursive_deserialize(*extract_val_from_str(right)) if right and right != NO_NODE else None
+    left =  recursive_deserialize(*extract_val_from_str(left)) if left and left != NO_NODE else None
+
+    return Node(val, right, left)
+
+def deserialize(str):
+    """
+    Deserialize a string into a tree.
+    """
+    print str
+
+    val, right, left = extract_val_from_str(str)
+    return recursive_deserialize(val, right, left) 
+    
 def main():
     print "Starting script"
+
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
 
     print "Success !"
 
