dac2ad93a80df43a7c97805577436bb632e9b485
Jolan Cornevin <jcornevin@work4labs.com>
2018-07-09 14:13:25 +0200

    add daily 3 problem file

---
+++ daily_3_serialise_tree.py
@@ -0,0 +1,26 @@
+"""
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+/!\ ASSERTIONS: 
+  - I can use regex.
+"""
+def main():
+    print "Starting script"
+
+    print "Success !"
+
+if __name__ == "__main__":
+    main()
