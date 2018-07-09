53088cc54004bc901599e5d19b01135b1aa3e04e
Jolan Cornevin <jcornevin@work4labs.com>
2018-07-09 14:24:45 +0200

    fix daily 3 problem after testing file

--- daily_3_serialise_tree.py
+++ daily_3_serialise_tree.py
@@ -28,12 +28,14 @@ class Node:
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return "{}: ({}, {})".format(self.val, self.left, self.right)
 
 def serialize(node):
     """
     Serializes the tree into a string. The format is (val, (right_node), (left_node)) recursivly.
     """
-    return '[{}, {}, {}]'.format(node.val, serialize(node.right) if node.right else NO_NODE, serialize(node.left) if node.left else NO_NODE)
+    return '[{}, {}, {}]'.format(node.val, serialize(node.left) if node.left else NO_NODE, serialize(node.right) if node.right else NO_NODE)
 
 def extract_val_from_str(str):
     if not  RE_NODE_PATTERN.match(str):
@@ -41,25 +43,24 @@ def extract_val_from_str(str):
 
     return RE_NODE_PATTERN.match(str).groups()
 
-def recursive_deserialize(val, right, left):
+def recursive_deserialize(val, left, right):
     right = recursive_deserialize(*extract_val_from_str(right)) if right and right != NO_NODE else None
     left =  recursive_deserialize(*extract_val_from_str(left)) if left and left != NO_NODE else None
 
-    return Node(val, right, left)
+    return Node(val, left, right)
 
 def deserialize(str):
     """
     Deserialize a string into a tree.
     """
-    print str
-
-    val, right, left = extract_val_from_str(str)
-    return recursive_deserialize(val, right, left) 
+    val, left, right = extract_val_from_str(str)
+    return recursive_deserialize(val, left, right) 
     
 def main():
     print "Starting script"
 
     node = Node('root', Node('left', Node('left.left')), Node('right'))
+
     assert deserialize(serialize(node)).left.left.val == 'left.left'
 
     print "Success !"
