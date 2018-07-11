a1577a80c3d3d77f7e07eb9066e01cd1b1c4f2ab
Jolan Cornevin <jcornevin@work4labs.com>
2018-07-11 18:16:09 +0200

    implement daily 7 ONE SHOT

--- daily_7_count_encoding_possibilities.py
+++ daily_7_count_encoding_possibilities.py
@@ -1,11 +1,31 @@
 """
-Insert exercise statement
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+You can assume that the messages are decodable. For example, '001' is not allowed.
 """
+
+def is_possible_pair(first_char, next_char):
+    if int(first_char + next_char) < 27:
+	return True
+    return False
+
+def count_decoded_possibilities(string, count):
+    if not string:
+	return count + 1
+	
+    if len(string) > 1 and is_possible_pair(string[0], string[1]):
+	count = count_decoded_possibilities(string[2:], count)
+    
+    return count_decoded_possibilities(string[1:], count)
+
 
 def main():
     print "Starting script"
 
-    # Insert code here
+    for example, expected_result in [('111', 3), ('12', 3)]:
+	assert count_decoded_possibilities(example, 0), expected_result
 
     print "Success !"
 
