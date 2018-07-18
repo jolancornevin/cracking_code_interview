876d50eda7c973e13da50986e41d29fe05e4bdc9
Jolan Cornevin <jcornevin@work4labs.com>
2018-07-18 18:56:47 +0200

    oopsie, use done instead of custom done condition

--- ai_gym/cart_pole.py
+++ ai_gym/cart_pole.py
@@ -2,24 +2,14 @@ import gym
 env = gym.make('CartPole-v0')
 env.reset()
 
-def is_over(state):
-    pos = state[0]
-    angle = state[2]
-
-    # The cart is too far from the center
-    if pos > 2.4 or pos < -2.4:
-        return True
-    if angle > 1.5 or angle < -1.5:
-        return True
-    return False
-
 for i in range(100):
     env.render()
     state, reward, done, _ = env.step(0) 
     
     # The cart is too far from the center
-    if is_over(state):
-        env.close()
+    if done:
         break
 
     print state
+
+env.done()
