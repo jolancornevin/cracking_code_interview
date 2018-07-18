956d6eb7ee5e328e5fad0231abbdc9c2fc4af806
Jolan Cornevin <jcornevin@work4labs.com>
2018-07-18 18:54:28 +0200

    add stop condition

--- ai_gym/cart_pole.py
+++ ai_gym/cart_pole.py
@@ -2,6 +2,24 @@ import gym
 env = gym.make('CartPole-v0')
 env.reset()
 
-for i in range(1000):
+def is_over(state):
+    pos = state[0]
+    angle = state[2]
+
+    # The cart is too far from the center
+    if pos > 2.4 or pos < -2.4:
+        return True
+    if angle > 1.5 or angle < -1.5:
+        return True
+    return False
+
+for i in range(100):
     env.render()
-    state, reward, done, _ = env.step(1) 
+    state, reward, done, _ = env.step(0) 
+    
+    # The cart is too far from the center
+    if is_over(state):
+        env.close()
+        break
+
+    print state
--- ai_gym/test.py
+++
@@ -1,7 +0,0 @@
-import gym
-env = gym.make('CartPole-v0')
-env.reset()
-
-for i in range(1000):
-    env.render()
-    state, reward, done, _ = env.step(1) 
